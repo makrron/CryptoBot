487265e6f925da13d8c8f22ad692896244e4cb60
makrron <mvazquezarenal@uma.es>
2024-03-07 13:07:37 +0100

    web

--- src/router.ts
+++ src/router.ts
@@ -4,7 +4,7 @@ import {makeThisModuleAnExecutableRouteLister} from "github-pages-plugin-for-typ
 
 
 export const routeDefs = {
-	"home": defineRoute("/CryptoBot/"),
+	"home": defineRoute("/"),
 	"pageExample": defineRoute("/page-example"),
 };
 
